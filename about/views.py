7b294a7ffd0406df351fc0b194985d265c33ab99
anti009x <riskisuleman76@gmail.com>
2024-02-16 15:55:03 +0700

    Update Validasi Dashboard

--- about/views.py
+++ about/views.py
@@ -2,8 +2,14 @@
 
 from django.shortcuts import render
 
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+
+
+@login_required(login_url=settings.LOGIN_URL)
 def index(request):
-    context = {
-        'name': 'Hello Woy',  
-    }
-    return render(request, 'index.html', context)
+    # context = {
+    #     'name': 'Hello Woy',  
+    # }
+    return render(request, 'index.html')
+
